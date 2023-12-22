e254c21bb48b73ff3fa48bdcd2699fb83f8cd4ac
raiiguilherme <raiguilherme322@gmail.com>
2023-12-21 23:53:18 -0300

    dando uma olhada em fila

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -2,5 +2,9 @@
 
 public class Pessoa
 {
+   private String nome; 
+
+    public String GetNome(){return nome;}
+    public void SetNome(String n){nome=n;}
 
 }
--- Program.cs
+++ Program.cs
@@ -3,21 +3,68 @@ using C______;
 
 Pessoa pp = new Pessoa();
 
-try
-{
-        string [] linhas = File.ReadAllLines("arquivos/oi.txt");
+Queue<int> fila = new Queue<int>();
 
-        foreach (string lin in linhas)
-     {
-         Console.WriteLine(lin);
+
+
+fila.Enqueue(1000);
+fila.Enqueue(10213);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
 }
 
-}
-catch (System.Exception e)
+Console.WriteLine("vamos remover um elemento agr");
+fila.Dequeue();
+
+Console.WriteLine("mostrando novamente a fila sem o item retirado");
+foreach (int item in fila)
 {
-    Console.WriteLine("Deu erro meu chapa "+ e.Message);
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try
+// {
+//         string [] linhas = File.ReadAllLines("arquivos/oi.txt");
+
+//         foreach (string lin in linhas)
+//      {
+//          Console.WriteLine(lin);
+// }
+
+// }
+// catch (System.Exception e)
+// {
+//     Console.WriteLine("Deu erro meu chapa, coloque a  "+ e.Message);
     
-}
+// }
 
 
 
